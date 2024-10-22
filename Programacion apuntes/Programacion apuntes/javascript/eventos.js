d96b8bbbbbe1fd1a2ef634b249196e6f1c4bb314
Camo93 <kmylobeta@gmail.com>
2024-10-22 11:57:52 -0500

    Delete Programacion apuntes/Programacion apuntes directory

--- Programacion apuntes/Programacion apuntes/javascript/eventos.js
+++
@@ -1,3 +0,0 @@
-function saludar(){
-    return alert(`hi`)
-}
--- Programacion apuntes/Programacion apuntes/javascript/index.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-    let $h1=document.querySelector(h1) //diferencia variables
-    let $p=document.querySelector("#par")
-    console.log($h1)
-    console.log($p)
-    console.log($h1.textContent)
-}
